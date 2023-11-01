c28f2e821830dee1e5efc60fefca1428f984056b
Huynguyen <huylearndata@gmail.com>
2023-11-01 19:07:24 +0700

    data ingestion notebook added

---
+++ main.py
@@ -0,0 +1,14 @@
+from Question_Answering.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Question_Answering.logging import logger
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/Question_Answering/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,15 @@
+from Question_Answering.config.configuration import ConfigurationManager
+from Question_Answering.components.data_ingestion import DataIngestion
+from Question_Answering.logging import logger
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
